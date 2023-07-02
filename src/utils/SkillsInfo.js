014525d7e3bc32b33ed6e0df6981178f6d2bf766
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-07-03 01:38:44 +0530

    Skilled name info with amount of knowledge info added

---
+++ src/utils/SkillsInfo.js
@@ -0,0 +1,67 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML 5",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "75",
+        },
+        {
+          skillName: "TailwindCss",
+          percentage: "50",
+        },
+        {
+          skillName: "React.js",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "Node.js",
+          percentage: "75",
+        },
+        {
+          skillName: "Express.js",
+          percentage: "70",
+        },
+        {
+          skillName: "MongoDB",
+          percentage: "75",
+        },
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+      ],
+    },
+    {
+      label: "Programming",
+      data: [
+        
+        {
+          skillName: "C",
+          percentage: "55",
+        },
+        {
+          skillName: "C++",
+          percentage: "85",
+        },
+        {
+          skillName: "JAVA",
+          percentage: "75",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "55",
+        },
+        
+      ],
+    },
+  ];
