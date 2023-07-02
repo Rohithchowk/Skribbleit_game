834afef3624e809792106a6c1acc23b5b984cc5a
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-07-02 12:49:55 +0530

    data for nav links added with link to page

---
+++ src/utils/Data.js
@@ -0,0 +1,33 @@
+ const Data=[
+    {
+        label:'Home',
+        to:'/'
+
+    },
+    {
+        label:'About me',
+        to:'/'
+
+    },
+    {
+        label:'Skills',
+        to:'/skills'
+
+    },
+    {
+        label:'Resume',
+        to:'/resume'
+
+    },
+    {
+        label:'Contact',
+        to:'/contact'
+
+    },
+    {
+        label:'Portfolio',
+        to:'/portfolio'
+
+    },
+];
+export default Data;
