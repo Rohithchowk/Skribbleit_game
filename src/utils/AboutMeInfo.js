796890b60cdc53a2344b57bfc59d659f8f8cbee7
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-07-02 23:48:27 +0530

    personal information added with a summary

---
+++ src/utils/AboutMeInfo.js
@@ -0,0 +1,26 @@
+export const personalDetails = [
+    {
+      label: "Name",
+      value: "Ankur Srivastava",
+    },
+    {
+      label: "Age",
+      value: "22",
+    },
+    {
+      label: "Address",
+      value: "India",
+    },
+    {
+      label: "Email",
+      value: "ankursrivastava2070@gmail.com",
+    },
+    {
+      label: "Contact No",
+      value: "+91 7355446956",
+    },
+  ];
+  
+export  const jobSummary =
+    "I am an enthusiastic fresher MERN stack developer with 2 years of experience. My primary goal is to create immersive user experiences through efficient website development practices. I have gained little knowledge and hands-on practice in technologies such as Tailwind CSS, React Native, and Docker, enabling me to handle diverse projects with confidence. I have a strong passion for open source and actively contribute to collaborative projects. With a solid foundation in web development principles, I prioritize clean and maintainable code. I excel in problem-solving and thrive in dynamic environments. Committed to delivering scalable solutions, I am eager to join a team of like-minded professionals to create exceptional user experiences and advance technology.";
+  
