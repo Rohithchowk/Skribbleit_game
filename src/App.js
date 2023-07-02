6265afbb0894accccb6d8cc64355105d16f6a8a6
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-07-02 12:51:43 +0530

    Pages with there routes are added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import {Routes,Route} from 'react-router-dom';
+import  './app.css';
+import Home from './Containers/Home';
+import About from './Containers/About';
+import Portfolio from './Containers/Portfolio';
+import Resume from './Containers/Resume';
+import Skills from './Containers/Skills';
+import Contact from './Containers/Contact';
+import Navbar from './Components/Navbar/index';
 function App() {
   return (
-    <div className="App">
-      hello
+    <div >
+    <Navbar></Navbar>
+    
+    <Routes>
+      <Route index path='/' element={<Home/>}/>
+      <Route  path='/about' element={<About/>}/>
+      <Route  path='/portfolio' element={<Portfolio/>}/>
+      <Route  path='/resume' element={<Resume/>}/>
+      <Route  path='/skills' element={<Skills/>}/>
+      <Route  path='/contact' element={<Contact/>}/>
+    </Routes>
     </div>
   );
 }
