92a24ab5da662f620eb6de20f7db4114ff24843b
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-07-02 17:18:16 +0530

    home component added with CV download option

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,6 +1,52 @@
-const Home=()=>{
-    return <div>
-        Home
-    </div>
+import React from "react";
+import { Animate } from "react-simple-animate";
+import "./style.css";
+import { Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+
+const Home = () => {
+    const navigate = useNavigate();
+const handleLink=()=>{
+    navigate('/contact');
+
 }
+const handleDownload = () => {
+  const downloadLink = document.createElement('a');
+  downloadLink.href = '/CV/ANKUR_SRIVASTAVA_RESUME.pdf'; 
+  downloadLink.download = 'ANKUR_SRIVASTAVA_RESUME.pdf'; 
+  downloadLink.click();
+
+};
+  return (
+    <section id="home" className="home">
+      <div className="home__text-wrapper">
+        <h1>
+          Hello, I'm Ankur
+          <br />
+          Mern stack developer
+        </h1>
+      </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <div className="home__contact-me">
+          <Stack direction="column">
+          <button onClick={handleLink}>Hire me</button>
+
+          <button onClick={handleDownload} className="cv">Download CV</button>
+          </Stack>
+        </div>
+      </Animate>
+    </section>
+  );
+};
 export default Home;
